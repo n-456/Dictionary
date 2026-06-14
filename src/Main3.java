9238b0cb7902d4a56ca8ecbd4278e232489a98f8
n-456 <n-456@users.noreply.github.com>
2026-06-14 14:50:44 +0700

    Task-US-1.3: Test search functions

--- src/Main3.java
+++ src/Main3.java
@@ -1,5 +1,8 @@
 import models.Dictionary;
+import models.Word;
 import repository.impl.WordGAO;
+
+import java.util.List;
 
 public class Main3 {
     public static void main(String[] args) {
@@ -11,5 +14,33 @@ public class Main3 {
 
         // Result of load from file
         dictionary.inOrder(dictionary.getRoot());
+
+
+        System.out.println("\n=====SEARCH SUBSTRING=====\n");
+        // Test search substring
+        String substring = "se";
+        List<Word> words = dictionary.searchSubstringWord(substring);
+        for (Word word : words) {
+            System.out.println("WORD CONTSAINS \"" + substring + "\": " + word);
+        }
+
+
+        System.out.println("\n=====SEARCH=====\n");
+        // Test search
+        String search1 = "and";
+        Word resultSearch1 = dictionary.searchWord(search1);
+        if (resultSearch1 == null) {
+            System.out.println("WORD 1: Can't found \"" + search1 + "\"");
+        } else {
+            System.out.println("WORD 1: " + resultSearch1);
+        }
+        // Test search
+        String search2 = "aim";
+        Word resultSearch2 = dictionary.searchWord(search2);
+        if (resultSearch2 == null) {
+            System.out.println("WORD 2: Can't found \"" + search2 + "\"");
+        } else {
+            System.out.println("WORD 2: " + resultSearch2);
+        }
     }
 }
