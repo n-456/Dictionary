9b997c8170a98b8e8bfc85535981c0bded9fc665
n-456 <n-456@users.noreply.github.com>
2026-06-20 15:06:29 +0700

    Task 1.3: main

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,52 @@
-//import models.Dictionary;
-//import models.Node;
-//import models.Word;
-//import service.WordService;
-//import repository.impl.WordGAO;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//
-//        //Test readAll()
-//        WordGAO wordGAO = new WordGAO();
-//        List<Word> words = wordGAO.readAll();
-//        //Print result
-//        System.out.println("---List hien tai---");
-//        for(Word word:words){
-//            System.out.println(word);
-//        }
-//
-//        //Test writeAll()
-//
-//
-//        Dictionary dictionary = new Dictionary();
-//
-//        System.out.println("=== 1. THỰC HIỆN THÊM CÁC NÚT (INSERT) ===");
-////
-////        dictionary.insert(new Word("cat"));
-////        dictionary.insert(new Word("bed"));
-////        dictionary.insert(new Word("ant"));
-////        dictionary.insert(new Word("hen"));
-////        dictionary.insert(new Word("see"));
-////        dictionary.insert(new Word("pet"));
-//
-//        //writeAll()
-//        wordGAO.writeAll(dictionary.getRoot());
-//
-//        //Print result
-//        System.out.println("---List sau khi them---");
-//        words = wordGAO.readAll();
-//        for(Word word:words){
-//            System.out.println(word);
-//        }
-//
-//    }
-//}
+import models.Dictionary;
+import models.TypeOfWord;
+import models.Word;
+import repository.impl.WordGAO;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Dictionary dictionary = new Dictionary();
+
+        System.out.println("\n=====LOAD FILE=====\n");
+        //Test load from fiile
+        WordGAO wordGAO = new WordGAO("resource/dictionary.json");
+        wordGAO.loadFromFile(dictionary);
+        // Result of load from file
+        dictionary.inOrder(dictionary.getRoot());
+
+        System.out.println("\n=====INSERT=====\n");
+        // Test insert
+        TypeOfWord type1 = new TypeOfWord("kiến","Noun","An ant");
+        Word wordInsert = new Word("ant",List.of(type1));
+        dictionary.insertWord(wordInsert);
+        System.out.println("Đã thêm từ 'ant'");
+
+        System.out.println("\n=====SEARCH=====\n");
+        // Test search
+        String search1 = "ant";
+        Word resultSearch1 = dictionary.searchWord(search1);
+        if (resultSearch1 == null) {
+            System.out.println("WORD 1: Can't found \"" + search1 + "\"");
+        } else {
+            System.out.println("WORD 1: " + resultSearch1);
+        }
+        // Test search
+        String search2 = "set of china";
+        Word resultSearch2 = dictionary.searchWord(search2);
+        if (resultSearch2 == null) {
+            System.out.println("WORD 2: Can't found \"" + search2 + "\"");
+        } else {
+            System.out.println("WORD 2: " + resultSearch2);
+        }
+
+        System.out.println("\n=====SEARCH SUBSTRING=====\n");
+        // Test search substring
+        String substring = "se";
+        List<Word> words = dictionary.searchSubstringWord(substring);
+        for (Word word : words) {
+            System.out.println("WORD CONTSAINS \"" + substring + "\": " + word);
+        }
+    }
+}
