95c9d2bf1d2fb73ab991d99b9c4906238f41c54e
n-456 <n-456@users.noreply.github.com>
2026-06-21 08:40:29 +0700

    Task 2.1: implement handle exception

--- src/exception/ExceptionHandler.java
+++ src/exception/ExceptionHandler.java
@@ -1,12 +1,28 @@
 package exception;
 
+import javax.swing.JOptionPane;
+
 public class ExceptionHandler {
+
     public static void handle(Exception e) {
+        String title = "Lỗi Hệ Thống";
+        String message = e.getMessage();
+        int messageType = JOptionPane.ERROR_MESSAGE;
+
         if (e instanceof NullPointerException) {
-            System.out.println("Lỗi dữ liệu: Đối tượng bị rỗng");
+            title = "Lỗi Dữ Liệu Rỗng";
+            messageType = JOptionPane.WARNING_MESSAGE;
         }
-        else {
-            System.out.println("Có lỗi xảy ra");
+        else if (e instanceof IllegalArgumentException) {
+            title = "Dữ Liệu Không Hợp Lệ";
+            messageType = JOptionPane.ERROR_MESSAGE;
         }
+
+        // In log lỗi
+        System.err.println("[LOG LỖI]: " + e.toString());
+        e.printStackTrace();
+
+        // Hiển thị hộp thoại popup lỗi cho người dùng xem
+        JOptionPane.showMessageDialog(null, message, title, messageType);
     }
 }
